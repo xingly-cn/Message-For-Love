d5611d111b46f858d5e9a7212a45f40765e98a89
方糖 <admin@asugar.cn>
2023-02-22 16:14:33 +0800

    Initial commit

---
+++ src/main/java/com/asugar/messageforlove/MessageForLoveApplication.java
@@ -0,0 +1,13 @@
+package com.asugar.messageforlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageForLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageForLoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asugar/messageforlove/MessageForLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asugar.messageforlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageForLoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
